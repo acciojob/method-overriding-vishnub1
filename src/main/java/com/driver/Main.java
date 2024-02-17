052fe069abf2a47eb708e9a29b9bd4aeb2890ac1
VISHNU <vishnubiradar576@gmail.com>
2024-02-17 17:16:57 +0530

    Interesting Activity by accio

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,15 +5,12 @@ STEPS:
 Method Overriding
 
 Task 1: create a class A, with a method named meth having returning a string "Invoking method from class A"
-
 Task 2: create a class B which extends class A
-
 Task 3: create object of class B and call method meth of class A with it
-
 Task 4: Now override methode meth of class A in class B, which returns the following string "Method is overridden in Extendend class B"
-
 Task 5: call this overridden method from obj of class B
 */
+
 class A {
     public String meth() {
         return "Invoking method from class A";
@@ -29,7 +26,7 @@ class B extends A {
 
 public class Main {
     public static void main(String[] args) {
-        B bobj = new B();
-        System.out.println(bobj.meth());
+        B objB = new B();
+        System.out.println(objB.meth());
     }
 }
